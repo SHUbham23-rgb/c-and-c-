500891bada5a855de68241a5744db624e949d3b5
SHUbham23-rgb <68004285+SHUbham23-rgb@users.noreply.github.com>
2020-07-09 19:54:05 +0530

    Add files via upload
    this is my second assement

---
+++ addition assingment.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{    
+    int number1,number2,number3,sum;
+    printf("Enter three integers: ");
+    scanf("%d %d %d",&number1,&number2,&number3);
+    sum = number1+number2+number3;      
+    printf("%d + %d +%d = %d", number1, number2, number3, sum);
+    return 0;
+}
